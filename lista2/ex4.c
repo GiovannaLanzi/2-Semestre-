7c2b3e6b21ac8a55c433782597290fb96e236026
Giovanna Lanzi Lopes Torres <160290107+GiovannaLanzi@users.noreply.github.com>
2024-11-05 14:24:35 -0300

    Add files via upload

---
+++ lista2/ex4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <locale.h>
+//Giovanna Lanzi Lopes Torres 
+
+int somat(int n); 
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int numero=0;
+	
+	printf("Digite um número: ");
+	scanf("%d", &numero);
+	
+	printf("Soma = %d", somatorio(numero));
+	
+	return 0;
+}
+
+int somatorio(int n){
+	int soma = 0, i;
+	for(i=1; i<=n; i++){
+		soma = soma + (5 * i * i + 2 * i + 8);
+	}
+	//printf("Soma = %d", soma(n));
+	return soma;
+}
