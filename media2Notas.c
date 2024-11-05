7afe529ab18e59a1f1ef9b31f91c010fb2244db9
Giovanna Lanzi Lopes Torres <160290107+GiovannaLanzi@users.noreply.github.com>
2024-11-05 14:33:38 -0300

    Create media2Notas.c

---
+++ media2Notas.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+	int n1, n2;
+	float media;
+	
+	printf("Digite duas notas: ");
+	scanf("%d %d", &n1, &n2);
+	
+	media = (n1+n2)/2.0;
+	printf("Média = %.1f\n", media);
+	
+	if(n1==n2)
+		printf("Notas iguais\n");
+	else if(n1>n2)
+		printf("N1 maior = %d", n1);
+	else
+		printf("N2 maior = %d", n2);
+		
+	return 0;
+}
